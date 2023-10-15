25af46493bc4226b99a0f6a133ff74b4e2c15276
hienbv1312 <joshien97@gmail.com>
2023-10-15 16:27:31 +0700

    update category filter

--- js/list-product.js
+++ js/list-product.js
@@ -4,28 +4,33 @@ const numberElOfPage = 15;
 
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
-
+let data;
 $(function () {
   let currentPage = 1;
+  let type = 1;
   if (urlParams.has("page")) {
     currentPage = +urlParams.get("page");
   }
+  if (urlParams.has("type")) {
+    type = +urlParams.get("type");
+  }
+  data = products.filter((item) => item.type == type);
   nextTo(currentPage);
-  const numberOfPage = Math.round(products.length / numberElOfPage);
+  const numberOfPage = Math.ceil(data.length / numberElOfPage);
   const arr = Array.from(Array(numberOfPage).keys());
 
   const pageItemView = document.getElementById("page-item-view");
   pageItemView.outerHTML = arr
     .map((item) => {
       const pageNumber = item + 1;
-      return `<li class="page-item"><a class="page-link page-item-number" href="list-product.html?page=${pageNumber}">${pageNumber}
+      return `<li class="page-item"><a class="page-link page-item-number" href="list-product.html?type=${type}&page=${pageNumber}">${pageNumber}
           </button></li>`;
     })
     .join("");
 });
 
 function nextTo(pageNumber) {
-  const result = products.slice(
+  const result = data.slice(
     (pageNumber - 1) * numberElOfPage,
     pageNumber * numberElOfPage
   );
@@ -51,4 +56,3 @@ function nextTo(pageNumber) {
     )
     .join("");
 }
-
