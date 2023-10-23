c1274debce5c2b8eaff41b304bed05767e74fd4e
hieu3557 <124872187+hieu3557@users.noreply.github.com>
2023-10-23 18:49:09 +0700

    23/10

--- js/product-detail.js
+++ js/product-detail.js
@@ -11,18 +11,30 @@ $(function () {
     const name = product.name;
     const description = product.description;
     const cost = product.cost;
-    const img = product.img
+    const img1 = product.img
+    const img2 = product.img
+    const img3 = product.img
+    const img4 = product.img
+    const img5 = product.img
 
     const elName = document.getElementById("product-name");
     const elDescription = document.getElementById("product-description");
     const elCost = document.getElementById("product-cost");
-    const elImg = document.getElementById("product-img");
+    const elImg1 = document.getElementById("product-img-1");
+    const elImg2 = document.getElementById("product-img-2");
+    const elImg3 = document.getElementById("product-img-3");
+    const elImg4 = document.getElementById("product-img-4");
+    const elImg5 = document.getElementById("product-img-5");
     document.getElementById('button-add-to-cart').onclick = addToCart;
 
     elName.innerHTML = name;
     elCost.innerHTML = cost;
     elDescription.innerHTML = description;
-    elImg.setAttribute('src', img)
+    elImg1.setAttribute('src', img1)
+    elImg2.setAttribute('src', img2)
+    elImg3.setAttribute('src', img3)
+    elImg4.setAttribute('src', img4)
+    elImg5.setAttribute('src', img5)
   }
 });
 
